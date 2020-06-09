463267965aa6004ff18d2c19a70445998f872851
Alex Wachira <4lexchi@live.com>
2020-06-10 02:59:58 +0300

    css fix and screenshoot update

--- src/frontend/static/frontend/script.js
+++ src/frontend/static/frontend/script.js
@@ -15,16 +15,16 @@ request.onload = function() {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.forEach(expectedParam => {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
       const h1 = document.createElement('h1')
-      h1.textContent = movie.title
+      h1.textContent = expectedParam.title
 
       const p = document.createElement('p')
-      movie.description = movie.description.substring(0, 300)
-      p.textContent = `${movie.description}...`
+      expectedParam.description = expectedParam.description.substring(0, 300)
+      p.textContent = `${expectedParam.description}...`
 
       container.appendChild(card)
       card.appendChild(h1)
